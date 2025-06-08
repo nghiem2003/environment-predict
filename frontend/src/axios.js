18ad6c5f6723fc43cc73941906b9282818a84ade
Nghiem Duy <nghiem.eo.bua.18@gmail.com>
2025-06-08 12:50:01 +0700

    env stuff

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -7,7 +7,7 @@ const getToken = () => {
 
 // Create an Axios instance with default headers
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your API base URL
+  baseURL: process.env.REACT_APP_API_URL, // Replace with your API base URL
 });
 
 // Add a request interceptor to add the token to headers before each request
